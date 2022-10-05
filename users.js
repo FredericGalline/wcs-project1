4df59b341e88834064691229b799bdbfe44751a6
fredericgalline <frederic@galline.fr>
2022-10-05 11:10:47 +0200

    Update users.php whith content loop

--- users.js
+++ users.js
@@ -17,9 +17,31 @@ function cards() {
     },
   ];
   for (let i = 0; i < users.length; i++) {
-    let card = `<h2>${users.name[i]} <span>${users.name[i]} </span></h2>`;
+    let card = `
+    <div class="card">
+          <div class="header">
+            <div class="image">
+              <img src="./img/user.png" />
+            </div>
+            <div class="userInfo">
+              <h2>${users.name[i]} <span>${users.lastName[i]} </span></h2>
+              <div class="spec">${users.bio[i]}</div>
+            </div>
+          </div>
+          <div class="content">
+            <div class="icon">
+              <a href="${users.github[i]}"><i class="fa-brands fa-github"></i></a>
+              <a href="${users.facebook[i]}"><i class="fa-brands fa-facebook"></i></a>
+              <a href="${users.twitter[i]}"><i class="fa-brands fa-twitter"></i></a>
+              <a href="${users.instagram[i]}"><i class="fa-brands fa-instagram"></i></a>
+              <a href="${users.discord[i]}"><i class="fa-brands fa-discord"></i></a>
+              <a href="${users.slack[i]}"><i class="fa-brands fa-slack"></i></a>
+              <a href=""><i class="fa-brands fa-whatsapp"></i></a>
+            </div>
+          </div>
+        </div>
+    `;
   }
-
   return cards;
 }
 
